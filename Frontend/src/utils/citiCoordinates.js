7902fe8dfd29c3ce6cf8749fa142ed59138bf300
shreyaskamble1612 <shreyaskamble393@gmail.com>
2026-03-16 18:47:03 +0530

    Local Data

---
+++ Frontend/src/utils/citiCoordinates.js
@@ -0,0 +1,10 @@
+export const cityCoords = {
+  Mumbai:[19.0760,72.8777],
+  Delhi:[28.7041,77.1025],
+  Bangalore:[12.9716,77.5946],
+  Pune:[18.5204,73.8567],
+  Hyderabad:[17.3850,78.4867],
+  Chennai:[13.0827,80.2707],
+  Kolkata:[22.5726,88.3639],
+  Ahmedabad:[23.0225,72.5714]
+};
